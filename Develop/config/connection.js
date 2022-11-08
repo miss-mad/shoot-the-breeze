97be4bb2c8c792ac5b8cc2c5dfdb0598b700fe96
miss-mad <mmlh.harris@gmail.com>
2022-11-07 23:19:04 -0500

    added comments; required routes

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 // when is thoughtsDB created and used? here?
-mongoose.connect("mongodb://127.0.0.1:27017/thoughtsDB", {
+mongoose.connect("mongodb://127.0.0.1:27017/thoughtsDB",
+// these options prevent warnings that we would get from mongodb otherwise
+{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
--- Develop/server.js
+++ Develop/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 // require mongoose connection
 const db = require("./config/connection");
+const routes = require("./controllers");
+
+// require models once they're created
 
 // PORT info
 const PORT = process.env.PORT || 3001;
@@ -11,8 +14,14 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// tell the express app to listen on our designated PORT with the open mongoose connection
+// use the routes in the controllers folder
+app.use(routes);
+
+// tell the express app to listen on our designated PORT with the open mongoose database connection
+// if the connection fails for whatever reason, we don't want to listen at the PORT
+// remember that db variable = mongoose.connection
 db.once("open", () => {
+  // now that we've connected to mongoose, listen and console.log the PORT message
   app.listen(PORT, () => {
     console.log(`Social Network API server running on port ${PORT}`);
   });
