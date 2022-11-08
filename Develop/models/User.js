6b8c383eeff13e07323b7efc49b09bc846b315af
miss-mad <mmlh.harris@gmail.com>
2022-11-07 23:20:03 -0500

    outlined the Thought and User models

--- Develop/models/User.js
+++ Develop/models/User.js
@@ -1,28 +1,35 @@
-/*
-User
+const { Schema, model } = require("mongoose");
 
-username
-
-    String
-    Unique
-    Required
-    Trimmed
-
-email
-
-    String
-    Required
-    Unique
-    Must match a valid email address (look into Mongoose's matching validation)
-
-thoughts
-
-    Array of _id values referencing the Thought model
-
-friends
-
-Array of _id values referencing the User model (self-reference)
-
-Schema Settings
-Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
-*/
+const userSchema = new Schema(
+  {
+    username: {
+      /*
+            String
+            Unique
+            Required
+            Trimmed
+            */
+    },
+    email: {
+      /*
+            String
+            Required
+            Unique
+            Must match a valid email address (look into Mongoose's matching validation)
+            */
+    },
+    thoughts: {
+      // Array of _id values referencing the Thought model
+    },
+    friends: {
+      // Array of _id values referencing the User model (self-reference)
+    },
+  },
+  {
+    // Schema Settings
+    // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
