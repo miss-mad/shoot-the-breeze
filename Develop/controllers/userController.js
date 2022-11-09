274f187c2ce18b2a1ee3ea2d579f316a1636706a
miss-mad <mmlh.harris@gmail.com>
2022-11-09 17:44:51 -0500

    better outline for controller files - db setup

--- Develop/controllers/userController.js
+++ Develop/controllers/userController.js
@@ -1,23 +1,31 @@
-/*
-/api/users
+const { User, Thought } = require("../models");
 
-GET all users
-GET a single user by its _id and populated thought and friend data
-POST a new user:
-example data
+// GET all users
+
+
+// GET a single user by its _id and populated thought and friend data
+
+
+// POST a new user
+/* example data
 {
   "username": "lernantino",
   "email": "lernantino@gmail.com"
 }
-PUT to update a user by its _id
-DELETE to remove user by its _id
-
-BONUS: Remove a user's associated thoughts when deleted.
-
------------------
-
-/api/users/:userId/friends/:friendId
-
-POST to add a new friend to a user's friend list
-DELETE to remove a friend from a user's friend list
 */
+
+
+// PUT to update a user by its _id
+
+
+// DELETE to remove user by its _id
+// BONUS: Remove a user's associated thoughts when deleted
+
+
+// POST to add a new friend to a user's friend list
+
+
+// DELETE to remove a friend from a user's friend list
+
+
+module.exports = {};
