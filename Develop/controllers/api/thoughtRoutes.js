95b54887073025e78cef562aae4420ca779acaba
miss-mad <mmlh.harris@gmail.com>
2022-11-10 15:51:47 -0500

    added imported functions to http request methods

--- Develop/controllers/api/thoughtRoutes.js
+++ Develop/controllers/api/thoughtRoutes.js
@@ -1,38 +1,46 @@
 const router = require("express").Router();
-const {} = require("../thoughtController"); // inside the object, require the functions created in thoughtController.js
+const {
+  getAllThoughts,
+  getOneThought,
+  createNewThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../thoughtController"); // inside the object, require the functions created in thoughtController.js
 
 // remember the "/api/thoughts" path is already specified
 
-// using .route() method on the express Router instead of .use() because .route() targets the specific HTTP request type we want to make (GET, POST, PUT DELETE). these requests are also chainable; I can chain them later to make the code cleaner
+// using .route() method on the express Router instead of .use() because .route() targets the specific HTTP request type we want to make (GET, POST, PUT DELETE).
 
-// need to pass imported functions into each http request once they're made
+// these requests are also chainable; I can chain them later to make the code cleaner
 
 // /api/thoughts
 // GET all thoughts
-router.route("/").get();
+router.route("/").get(getAllThoughts);
 
 // /api/thoughts/:thoughtId
 // GET a single thought by its _id
-router.route("/:thoughtId").get();
+router.route("/:thoughtId").get(getOneThought);
 
 // /api/thoughts
 // POST a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-router.route("/").post();
+router.route("/").post(createNewThought);
 
 // /api/thoughts/:thoughtId
 // PUT to update a thought by its _id
-router.route("/:thoughtId").put();
+router.route("/:thoughtId").put(updateThought);
 
 // /api/thoughts/:thoughtId
 // DELETE to remove a thought by its _id
-router.route("/:thoughtId").delete();
+router.route("/:thoughtId").delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
 // POST to create a reaction stored in a single thought's reactions array field
-router.route("/:thoughtId/reactions").post();
+router.route("/:thoughtId/reactions").post(createReaction);
 
-// /api/thoughts/:thoughtId/reactions
+// /api/thoughts/:thoughtId/reactions/:reactionId
 // DELETE to pull and remove a reaction by the reaction's reactionId value
-router.route("/:thoughtId/reactions").delete();
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
--- Develop/controllers/api/userRoutes.js
+++ Develop/controllers/api/userRoutes.js
@@ -1,38 +1,46 @@
 const router = require("express").Router();
-const {} = require("../userController"); // inside the object, require the functions created in userController.js
+const {
+  getAllUsers,
+  getOneUser,
+  createNewUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../userController"); // inside the object, require the functions created in userController.js
 
 // remember the "/api/users" path is already specified
 
-// using .route() method on the express Router instead of .use() because .route() targets the specific HTTP request type we want to make (GET, POST, PUT DELETE). these requests are also chainable; I can chain them later to make the code cleaner
+// using .route() method on the express Router instead of .use() because .route() targets the specific HTTP request type we want to make (GET, POST, PUT DELETE).
 
-// need to pass imported functions into each http request once they're made
+// these requests are also chainable; I can chain them later to make the code cleaner
 
 // /api/users
 // GET all users
-router.route("/").get();
+router.route("/").get(getAllUsers);
 
 // /api/users/:userId
 // GET a single user by its _id and populated thought and friend data
-router.route("/:userId").get();
+router.route("/:userId").get(getOneUser);
 
 // /api/users
 // POST a new user
-router.route("/"),post();
+router.route("/").post(createNewUser);
 
 // /api/users/:userId
 // PUT to update a user by its _id
-router.route("/:userId").put();
+router.route("/:userId").put(updateUser);
 
 // /api/users/:userId
 // DELETE to remove user by its _id
-router.route("/:userId").delete();
+router.route("/:userId").delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
 // POST to add a new friend to a user's friend list
-router.route("/:userId/friends/:friendId").post();
+router.route("/:userId/friends/:friendId").post(addFriend);
 
 // /api/users/:userId/friends/:friendId
 // DELETE to remove a friend from a user's friend list
-router.route("/userId/friends/:friendsId").delete();
+router.route("/userId/friends/:friendsId").delete(deleteFriend);
 
 module.exports = router;
