c58a1d8c3fa4709570da34c6df8cd666141359b7
miss-mad <mmlh.harris@gmail.com>
2022-11-10 15:49:34 -0500

    created functions to manipulate data

--- Develop/seeds/data.js
+++ Develop/seeds/data.js
@@ -24,20 +24,56 @@ const thoughts = [
   "thought 10",
 ];
 
+const reactions = [
+  "reaction 1",
+  "reaction 2",
+  "reaction 3",
+  "reaction 4",
+  "reaction 5",
+  "reaction 6",
+  "reaction 7",
+  "reaction 8",
+  "reaction 9",
+  "reaction 10",
+];
+
 const findRandomArrayItem = (array) => {
   return array[Math.floor(Math.random() * array.length)];
 };
 
-const findRandomUser = () => `${findRandomArrayItem(users)}`;
+const findRandomUser = () => {
+  return `${findRandomArrayItem(users)}`;
+};
+
+const findMultipleRandomUsers = (numberOfRandomUsers) => {
+  const randomUsersArray = [];
+  for (let i = 0; i < numberOfRandomUsers; i++) {
+    randomUsersArray.push(findRandomUser());
+  }
+  return randomUsersArray;
+};
 
 const findRandomThought = (thoughtNumber) => {
   const thoughtsArray = [];
-  for (let i = 0; i < thoughtsArray; i++) {
+  for (let i = 0; i < thoughtNumber; i++) {
     thoughtsArray.push({ thoughtText: findRandomArrayItem(thoughts) });
   }
   return thoughtsArray;
 };
 
-module.exports = { findRandomUser, findRandomThought };
+const findRandomReaction = (reactionNumber) => {
+  const reactionsArray = [];
+  for (let i = 0; i < reactionNumber; i++) {
+    reactionsArray.push({ reactionBody: findRandomArrayItem(reactions) });
+  }
+  return reactionsArray;
+};
+
+module.exports = {
+  findRandomUser,
+  findMultipleRandomUsers,
+  findRandomThought,
+  findRandomReaction,
+};
 
 // module.exports = [users, thoughts];
